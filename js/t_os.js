231f9135f13d5bb4337e148255ed1facfcadc702
Electron7-7 <teahaile2@teahaile.com>
2025-05-07 22:29:16 -0400

    fixed fonts and added back functionality to the blag button

--- js/t_os.js
+++ js/t_os.js
@@ -15,15 +15,15 @@ const stingers = [
 
 $(document).ready(function()
 {
-	/*$('#blog-embed').load('/blag/blag.html', function()
+	$('#blog-embed').load('/blag/blag.html', function()
 	{
 		$('template').remove();
-	});*/
+	});
 
-	/*$('button[name="blog"]').on("click", function()
+	$('button[name="blog"]').on("click", function()
 	{
 		window.location.href = 'https://teahaile.com/blag';
-	});*/
+	});
 
 	let stinger = stingers[Math.floor(Math.random() * stingers.length)];
 
