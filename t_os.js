6e1219a19135108258f7994de94f1cb9aa2d70ee
Electron7-7 <teahaile2@teahaile.com>
2025-05-07 22:22:02 -0400

    updated stringer and added some more stingers

--- t_os.js
+++ t_os.js
@@ -1,5 +1,34 @@
+const stingers = [
+ "copyright lololol",
+ "don't try any of this at home",
+ "you heard him, don't do it!",
+ "reality is just two ts away from being realtitty",
+ "limeade > lemonade... sorry, but it's true",
+ "and if you thought <i>that</i> was bad...",
+ "teahaile1 is RogerOThornhill, confirmed!!!",
+ "I miss the 00s...",
+ // "check out the Wii theme",
+ "I love my online boyfriend :3",
+ ":3c",
+ "<|:38"
+]
+
 $(document).ready(function()
 {
+	/*$('#blog-embed').load('/blag/blag.html', function()
+	{
+		$('template').remove();
+	});*/
+
+	/*$('button[name="blog"]').on("click", function()
+	{
+		window.location.href = 'https://teahaile.com/blag';
+	});*/
+
+	let stinger = stingers[Math.floor(Math.random() * stingers.length)];
+
+	$('.stinger').append("\"" + stinger + "\"");
+
 	$.ajax(
 	{
 		url : "blag.txt",
